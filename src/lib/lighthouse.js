c72147342386967af9ab3d6a1851835aa4fcfa74
Colby Fayock <fayock@gmail.com>
2020-05-19 20:18:12 -0400

    adding reports to gitignore

--- src/lib/lighthouse.js
+++ src/lib/lighthouse.js
@@ -1,4 +1,3 @@
-const { exec } = require('child_process');
 const fs = require('fs');
 const psi = require('psi');
 
@@ -30,48 +29,6 @@ async function performLighthouseAudit({ url, outputDirectory } = {}) {
 }
 
 module.exports.performLighthouseAudit = performLighthouseAudit;
-
-/**
- * buildLighthouseCommand
- */
-
-function buildLighthouseCommand({ url, chromeFlags, outputType, outputPath }) {
-  const args = [];
-
-  if ( chromeFlags ) {
-    args.push(`--chrome-flags="${chromeFlags}"`);
-  }
-
-  if ( url ) {
-    args.push(url);
-  }
-
-  if ( outputType ) {
-    args.push(`--output="${outputType}"`);
-  }
-
-  if ( outputType ) {
-    args.push(`--output-path="${outputPath}"`);
-  }
-
-  return `${LIGHTHOUSE_EXECUTABLE} ${args.join(' ')}`;
-}
-
-/**
- * promiseToExec
- */
-
-function promiseToExec(command) {
-  return new Promise((resolve, reject) => {
-    exec(command, (error, stdout, stderr) => {
-      if (error) {
-        reject(error);
-        return;
-      }
-      resolve(stdout, stderr);
-    });
-  })
-}
 
 /**
  * promiseToCreateFile
